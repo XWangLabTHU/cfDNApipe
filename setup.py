d682648f125ff222c4ab6d03daa1c68fa7636173
ZweiTHU <zwfanwz678@gmail.com>
2019-11-01 11:19:48 +0800

    add details

--- setup.py
+++ setup.py
@@ -5,16 +5,26 @@ Created on Fri Aug  9 11:37:46 2019
 @author: zhang
 """
 
-from setuptools import setup
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
-    name='cfDNApipe',
-    version='0.0.1',
-    description='An Intergrated Pipeline For cfDNA Sequencing Data',
-    license='SEE LICENCE',
-    packages=['cfDNApipe'],
-    author='Zhang Wei',
-    author_email='w-zhang16@mail.tsinghua.edu.cn',
-    keywords=['cfDNA', 'WGS', 'WGBS'],
-    url='https://github.com/Honchkrow/cfDNApipe'
+    name = "cfDNApipe",
+    version = "0.0.2",
+    author = "Wei Zhang",
+    author_email = "w-zhang16@mail.tsinghua.edu.cn",
+    description = "An Intergrated Pipeline For cfDNA Sequencing Data",
+    license = "Please see LICENSE.txt.",
+    keywords = ['cfDNA', 'WGS', 'WGBS'],
+    url = "https://github.com/Honchkrow/cfDNApipe",
+    packages = find_packages(),
+	package_data = {'cfDNApipe': ['data/*']},
+    long_description = read('README.rst'),
+    platforms = "Linux/Unix, macOS"
 )
