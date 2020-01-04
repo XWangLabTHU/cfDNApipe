21e7bc69ea3edb2ac4b1a5209c2669f770a0832a
ZweiTHU <zwfanwz678@gmail.com>
2020-01-04 12:14:01 +0800

    change default parameters

--- cfDNApipe/Fun_bismarkmethylex.py
+++ cfDNApipe/Fun_bismarkmethylex.py
@@ -9,7 +9,7 @@ E-mail: huangjq16@mails.tsinghua.edu.cn
 
 from .StepBase import StepBase
 from .cfDNA_utils import commonError
-import os, re
+import os, math
 from .Configure import Configure
 
 
@@ -20,14 +20,16 @@ class bismark_methylation_extractor(StepBase):
     def __init__(self, 
              bamInput = None, # list
              outputdir = None, # str
-             genomedir = None,
              threads = 1,
-             other_params = {'--no_overlap': True, '--report': True, 
-                             '--no_header': True, '--gzip': True, '--multicore': 4, 
-                             '--cytosine_report': True, '--zero_based': True},
+             other_params = {'--no_overlap': True, '--report': True,
+                             '--no_header': True, '--gzip': True,
+                             '--bedGraph': True, '--zero_based': True},
+             paired = True,
              upstream = None,
              formerrun = None,
              **kwargs):
+        '''
+        '''
         if upstream is None:
             super(bismark_methylation_extractor, self).__init__()
             self.setInput('bamInput', bamInput)
@@ -39,6 +41,11 @@ class bismark_methylation_extractor(StepBase):
                 self.setOutput('outputdir', outputdir)
                 
             self.setParam('threads', threads)
+            
+            if paired:
+                self.setParam('type', 'paired')
+            else:
+                self.setParam('type', 'single')
             
         else:
             # here to check upstream source!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -56,14 +63,6 @@ class bismark_methylation_extractor(StepBase):
            
             self.setParam('type', Configure.getType())
             
-            if self.getParam('type') == 'paired':
-                other_params.update({'--paired': True})
-            elif self.setParam('type') == 'single':
-                other_params.update({'--single': True})
-            else:
-                commonError("Wrong data type, must be 'single' or 'paired'!")
-            
-            
             if upstream.__class__.__name__ == 'bismark' or 'bismark_deduplicate':
                 self.setInput('bamInput', upstream.getOutput('bamOutput'))
             else:
@@ -72,27 +71,32 @@ class bismark_methylation_extractor(StepBase):
             self.setOutput('outputdir', self.getStepFolderPath())
             self.setParam('threads', Configure.getThreads())
         
-    
         if other_params is None:
             self.setParam('other_params', '')
         else:
             self.setParam('other_params',  other_params)
+        
+        if self.getParam('type') == 'paired':
+            other_params.update({'--paired': True})
+        elif self.setParam('type') == 'single':
+            other_params.update({'--single': True})
+        else:
+            commonError("Wrong data type, must be 'single' or 'paired'!")
             
-        self.setOutput('gzOutput', [self.getOutput('outputdir') + '/' + x.split('/')[-1][ : -3] + 'CpG_report.txt.gz' for x in self.getInput('bamInput')])
-        
-        self.setInput('genomedir', Configure.getRefDir())
-        
-        multi_run_len = len(self.getInput('bamInput'))
+        other_params.update({'--multicore': math.ceil(self.getParam('threads') / 3)})
+        other_params.update({'--output': self.getOutput('outputdir')})
+            
+        self.setOutput('covOutput', [os.path.join(self.getOutput('outputdir'), os.path.splitext(os.path.basename(x))[0]) + '.bedGraph.gz.bismark.zero.cov' for x in self.getInput('bamInput')])
+        self.setOutput('bedGraphOutput', [os.path.join(self.getOutput('outputdir'), os.path.splitext(os.path.basename(x))[0]) + '.bedGraph.gz' for x in self.getInput('bamInput')])
+        self.setOutput('covgzOutput', [os.path.join(self.getOutput('outputdir'), os.path.splitext(os.path.basename(x))[0]) + '.bismark.cov.gz' for x in self.getInput('bamInput')])
+        self.setOutput('reportOutput', [os.path.join(self.getOutput('outputdir'), os.path.splitext(os.path.basename(x))[0]) + '_splitting_report.txt' for x in self.getInput('bamInput')])
         
         all_cmd = []
         
-        for i in range(multi_run_len):
-            tmp_cmd = self.cmdCreate(["bismark_methylation_extractor", 
-                                       self.getParam('other_params'),
-                                       '--output', self.getOutput('outputdir'),
-                                       '--genome_folder', self.getInput('genomedir'),
-                                       self.getInput('bamInput')[i]])
-            all_cmd.append(tmp_cmd)
+        tmp_cmd = self.cmdCreate(["bismark_methylation_extractor", 
+                                  self.getParam('other_params'),
+                                  self.getInput('bamInput')])
+        all_cmd.append(tmp_cmd)
         
         self.setParam('cmd', all_cmd)
         
