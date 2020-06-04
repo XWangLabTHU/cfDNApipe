96f2421521ad50b2110b78e391f00e0113a09901
Honchkrow <shaway1012@gmail.com>
2020-06-04 22:23:54 +0800

    update

--- cfDNApipe/__init__.py
+++ cfDNApipe/__init__.py
@@ -8,10 +8,6 @@ Created on Mon Aug 12 10:03:52 2019
 from .cfDNA_utils import *
 from .Configure import *
 from .Configure2 import *
-from .Pipeline import *
-from .StepBase import *
-from .report_generator import *
-from .report_generator_comp import *
 
 from .Fun_adapterremoval import *
 from .Fun_addRG import *
@@ -23,8 +19,12 @@ from .Fun_baseRecalibrator import *
 from .Fun_bismark import *
 from .Fun_bismarkdedup import *
 from .Fun_bismarkmethylex import *
+from .Fun_BisulfiteCountCovariates import *
+from .Fun_BisulfiteGenotyper import *
+from .Fun_BisulfiteTableRecalibration import *
 from .Fun_bowtie2 import *
 from .Fun_BQSR import *
+from .Fun_BSVF import *
 from .Fun_calcmethyl_v2 import *
 from .Fun_classcifier import *
 from .Fun_CNV import *
@@ -55,7 +55,21 @@ from .Fun_mutect2t import *
 from .Fun_OCF import *
 from .Fun_OCFplot import *
 from .Fun_PCA import *
+from .Fun_prinseq import *
 from .Fun_qualimap import *
 from .Fun_rmDuplicate import *
+from .Fun_samtofastq import *
+from .Fun_seqtk import *
 from .Fun_sequenceTrans import *
+from .Fun_unmapfasta import *
+from .Fun_VCFpostprocess import *
+from .Fun_virusbismark import *
+from .Fun_virusdetect import *
+from .Fun_virusID_extract import *
 from .Fun_WPS import *
+
+from .Pipeline import *
+from .report_generator_comp import *
+from .report_generator import *
+from .StepBase import *
+from .StepBase2 import *
