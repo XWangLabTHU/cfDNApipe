e19d03b62ebf680efaaff2f730506ad022ea8492
ZweiTHU <zwfanwz678@gmail.com>
2020-12-01 09:47:38 +0800

    Update __init__.py

--- cfDNApipe/__init__.py
+++ cfDNApipe/__init__.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Aug 12 10:03:52 2019
 
-@author: zhang
+@author: zhang wei
 """
 
 from .cfDNA_utils import *
@@ -21,14 +21,9 @@ from .Fun_bcftoolsVCF import *
 from .Fun_bismark import *
 from .Fun_bismarkdedup import *
 from .Fun_bismarkmethylex import *
-from .Fun_BisulfiteCountCovariates import *
-from .Fun_BisulfiteGenotyper import *
-from .Fun_BisulfiteTableRecalibration import *
 from .Fun_bowtie2 import *
 from .Fun_BQSR import *
-from .Fun_BSVF import *
 from .Fun_calcmethyl_v2 import *
-from .Fun_classcifier import *
 from .Fun_CNV import *
 from .Fun_cnvbatch import *
 from .Fun_cnvHeatmap import *
@@ -37,38 +32,22 @@ from .Fun_cnvTable import *
 from .Fun_compressmethyl import *
 from .Fun_contamination import *
 from .Fun_counter import *
-from .Fun_createPON import *
-from .Fun_dbimport import *
-from .Fun_DeconCCN import *
-from .Fun_DMR import *
 from .Fun_fastqc import *
 from .Fun_filterMutectCalls import *
 from .Fun_fpcount import *
 from .Fun_fpplot import *
 from .Fun_fragLen import *
 from .Fun_fragLencomp import *
-from .Fun_gatherVCF import *
 from .Fun_GCcorrect import *
 from .Fun_getPileup import *
 from .Fun_mapQ_filter import *
 from .Fun_identifyAdapter import *
 from .Fun_inputProcess import *
-from .Fun_mutect2n import *
 from .Fun_mutect2t import *
 from .Fun_OCF import *
 from .Fun_OCFplot import *
-from .Fun_PCA import *
-from .Fun_prinseq import *
 from .Fun_qualimap import *
 from .Fun_rmDuplicate import *
-from .Fun_samtofastq import *
-from .Fun_seqtk import *
-from .Fun_unmapfasta import *
-from .Fun_VCFpostprocess import *
-from .Fun_virusbismark import *
-from .Fun_virusdetect import *
-from .Fun_virusID_extract import *
-from .Fun_WPS import *
 
 from .Pipeline import *
 from .report_generator_comp import *
