c92ca21c5a023e4d3010bfbaf4672909a383e53d
ZweiTHU <zwfanwz678@gmail.com>
2020-12-02 17:11:02 +0800

    update

--- cfDNApipe/__init__.py
+++ cfDNApipe/__init__.py
@@ -24,6 +24,7 @@ from .Fun_bismarkmethylex import *
 from .Fun_bowtie2 import *
 from .Fun_BQSR import *
 from .Fun_calcmethyl_v2 import *
+from .Fun_classcifier import *
 from .Fun_CNV import *
 from .Fun_cnvbatch import *
 from .Fun_cnvHeatmap import *
@@ -32,6 +33,8 @@ from .Fun_cnvTable import *
 from .Fun_compressmethyl import *
 from .Fun_contamination import *
 from .Fun_counter import *
+from .Fun_DeconCCN import *
+from .Fun_DMR import *
 from .Fun_fastqc import *
 from .Fun_filterMutectCalls import *
 from .Fun_fpcount import *
@@ -40,14 +43,17 @@ from .Fun_fragLen import *
 from .Fun_fragLencomp import *
 from .Fun_GCcorrect import *
 from .Fun_getPileup import *
-from .Fun_mapQ_filter import *
 from .Fun_identifyAdapter import *
 from .Fun_inputProcess import *
+from .Fun_mutect2n import *
 from .Fun_mutect2t import *
 from .Fun_OCF import *
 from .Fun_OCFplot import *
+from .Fun_PCA import *
 from .Fun_qualimap import *
 from .Fun_rmDuplicate import *
+from .Fun_virusdetect import *
+from .Fun_WPS import *
 
 from .Pipeline import *
 from .report_generator_comp import *
