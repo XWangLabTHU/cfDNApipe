354ea385c27ae4a942ee307da61ba8374462f8e1
ZweiTHU <zwfanwz678@gmail.com>
2021-01-03 20:53:54 +0800

    remove wrong name

--- cfDNApipe/Fun_bismarkdedup.py
+++ cfDNApipe/Fun_bismarkdedup.py
@@ -56,7 +56,7 @@ class bismark_deduplicate(StepBase):
         else:
             Configure.configureCheck()
             upstream.checkFilePath()
-            if upstream.__class__.__name__ in ["bismark", "virusbismark"]:
+            if upstream.__class__.__name__ in ["bismark"]:
                 self.setInput("bamInput", upstream.getOutput("bamOutput"))
             else:
                 raise commonError("Parameter upstream must from bismark.")
